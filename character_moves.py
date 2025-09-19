d92747e22e3a6d2a7fe6f81dd7081583e0746f64
25-0308 <00rhkrtmdwns@hanmail.net>
2025-09-19 16:53:27 +0900

    3단계 루프화 및 삼각운동 추가

--- character_moves.py
+++ character_moves.py
@@ -12,7 +12,8 @@ character_x = 400
 character_y = 90
 running = True
 
-turn = True
+turn = 1
+triangle_dir=1
 degree=-90
 r = math.sqrt(150**2+150**2)
 
@@ -21,7 +22,7 @@ while running:
 
     background.draw(400, 30)
     character.draw(character_x, character_y)
-    if turn:
+    if turn==1:
         if character_x <= 780 and character_y <= 90:
             character_y=90
             character_x += 10
@@ -36,8 +37,9 @@ while running:
             character_y -= 10
 
         if(character_x==400 and character_y==90):
-            turn=False
-    elif not turn:
+            turn=2
+
+    elif turn==2:
         character_x=400+r*math.cos(math.radians(degree))*-1
         character_y=300+r*math.sin(math.radians(degree))
         degree+=3
@@ -45,7 +47,31 @@ while running:
             character_x=400
             character_y=90
             degree=-90
-            turn=True
+            turn=3
+
+    elif turn==3:
+        if triangle_dir==1:
+            character_x += 10
+            if character_x >= 780:
+                triangle_dir=2
+        elif triangle_dir==2:
+            character_x -= 8.5
+            character_y += 10
+            if character_y >= 550:
+                triangle_dir=3
+        elif triangle_dir==3:
+            character_x -= 8.5
+            character_y -= 10
+            if character_y <= 90:
+                triangle_dir=4
+        elif triangle_dir==4:
+            character_y=90
+            character_x += 10
+            if character_x >= 400 and character_y == 90:
+                triangle_dir=1
+                turn=1
+                character_x=400
+                character_y=90
 
     update_canvas()
     delay(0.01)
